80d587794b8728555907d6035187bcf065f9bed2
robotanarchy <robotanarchy@bingo-ev.de>
2015-03-03 23:34:53 +0100

    stats.sh saves the stats now

--- src/stats.sh
+++ src/stats.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 echo "--------------------"
 echo "        INPUT"
 echo "--------------------"
@@ -15,3 +16,20 @@ echo "--------------------"
 echo "       OUTPUT"
 echo "--------------------"
 du -c out/ucty.bin out/ucty.js
+
+
+# create a "stats" folder to keep track
+# of the stats. A new file gets created whenever
+# the repo doesn't have any changes and a stats
+# file of the current revision doesn't exist yet.
+if [ -d "stats" ] \
+	&& git diff-index --quiet HEAD -- \
+	&& "$1" == ""
+then
+	output="stats/$(git rev-parse HEAD).txt"
+	date > "$output"
+	$0 "savingoutput" > "$output"
+	
+	echo "--------------------"
+	echo "=> Stats saved to: $output"
+fi
